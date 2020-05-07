1f5fd6c619b008382e6f254567da17e1040feab1
Wrong Wang <wangbudui@foxmail.com>
2020-05-07 18:14:13 +0800

    rewrite json.go

--- isso/response/json/json.go
+++ isso/response/json/json.go
@@ -8,7 +8,6 @@ package json
 
 import (
 	"encoding/json"
-	"errors"
 	"net/http"
 
 	"wrong.wang/x/go-isso/isso/response"
@@ -17,69 +16,58 @@ import (
 
 const contentTypeHeader = `application/json`
 
-// OK creates a new JSON response with a 200 status code.
-func OK(builder response.Builder, body interface{}) {
+func writeJSON(builder response.Builder, body interface{}, status int) {
 	builder.WithHeader("Content-Type", contentTypeHeader)
+	builder.WithStatus(status)
 	builder.WithBody(toJSON(body))
 	builder.Write()
+}
+
+func writeErrorJSON(builder response.Builder, err error, status int) {
+	if err != nil {
+		writeJSON(builder, map[string]string{"error": err.Error()}, status)
+	}
+	writeJSON(builder, map[string]string{"error": http.StatusText(status)}, status)
+}
+
+// OK creates a new JSON response with a 200 status code.
+func OK(builder response.Builder, body interface{}) {
+	writeJSON(builder, body, http.StatusOK)
 }
 
 // Created sends a created response to the client.
 func Created(builder response.Builder, body interface{}) {
-	builder.WithStatus(http.StatusCreated)
-	builder.WithHeader("Content-Type", contentTypeHeader)
-	builder.WithBody(toJSON(body))
-	builder.Write()
+	writeJSON(builder, body, http.StatusCreated)
+}
+
+// Accepted sends a created response to the client.
+func Accepted(builder response.Builder, body interface{}) {
+	writeJSON(builder, body, http.StatusAccepted)
 }
 
 // ServerError sends an internal error to the client.
 func ServerError(builder response.Builder, err error) {
-	builder.WithError(err)
-	builder.WithStatus(http.StatusInternalServerError)
-	builder.WithHeader("Content-Type", contentTypeHeader)
-	builder.WithBody(toJSONError(err))
-	builder.Write()
+	writeErrorJSON(builder, err, http.StatusInternalServerError)
 }
 
 // BadRequest sends a bad request error to the client.
 func BadRequest(builder response.Builder, err error) {
-	builder.WithError(err)
-	builder.WithStatus(http.StatusBadRequest)
-	builder.WithHeader("Content-Type", contentTypeHeader)
-	builder.WithBody(toJSONError(err))
-	builder.Write()
+	writeErrorJSON(builder, err, http.StatusBadRequest)
 }
 
 // Unauthorized sends a not authorized error to the client.
 func Unauthorized(builder response.Builder) {
-	builder.WithStatus(http.StatusUnauthorized)
-	builder.WithHeader("Content-Type", contentTypeHeader)
-	builder.WithBody(toJSONError(errors.New("Access Unauthorized")))
-	builder.Write()
+	writeErrorJSON(builder, nil, http.StatusUnauthorized)
 }
 
 // Forbidden sends a forbidden error to the client.
 func Forbidden(builder response.Builder) {
-	builder.WithStatus(http.StatusForbidden)
-	builder.WithHeader("Content-Type", contentTypeHeader)
-	builder.WithBody(toJSONError(errors.New("Access Forbidden")))
-	builder.Write()
+	writeErrorJSON(builder, nil, http.StatusForbidden)
 }
 
 // NotFound sends a page not found error to the client.
 func NotFound(builder response.Builder) {
-	builder.WithStatus(http.StatusNotFound)
-	builder.WithHeader("Content-Type", contentTypeHeader)
-	builder.WithBody(toJSONError(errors.New("Resource Not Found")))
-	builder.Write()
-}
-
-func toJSONError(err error) []byte {
-	type errorMsg struct {
-		ErrorMessage string `json:"error_message"`
-	}
-
-	return toJSON(errorMsg{ErrorMessage: err.Error()})
+	writeErrorJSON(builder, nil, http.StatusNotFound)
 }
 
 func toJSON(v interface{}) []byte {
