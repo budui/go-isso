031c77ae196949633ca17006a47e3d7962b892a6
Wrong Wang <wangbudui@foxmail.com>
2020-05-18 21:15:43 +0800

    use correct regex in router

--- server/api.go
+++ server/api.go
@@ -21,11 +21,11 @@ func registerRoute(router *mux.Router, isso *isso.ISSO) {
 	router.HandleFunc("/id/{id:[0-9]+}", isso.ViewComment()).Methods("GET").Name("view")
 	router.HandleFunc("/id/{id:[0-9]+}", isso.EditComment()).Methods("PUT").Name("edit")
 	router.HandleFunc("/id/{id:[0-9]+}", isso.DeleteComment()).Methods("DELETE").Name("delete")
-	router.HandleFunc("/id/{id:[0-9]+}/{vote:[like|dislike]}", isso.VoteComment()).Methods("POST").Name("vote")
+	router.HandleFunc("/id/{id:[0-9]+}/{vote:(?:like|dislike)}", isso.VoteComment()).Methods("POST").Name("vote")
 
-	router.HandleFunc("/id/{id:[0-9]+}/{action:[edit|activate|delete]}/{key}", workInProcess).
+	router.HandleFunc("/id/{id:[0-9]+}/{action:(?:edit|activate|delete)}/{key}", workInProcess).
 		Methods("GET").Name("moderate_get")
-	router.HandleFunc("/id/{id:[0-9]+}/{action:[edit|activate|delete]}>/{key}", workInProcess).
+	router.HandleFunc("/id/{id:[0-9]+}/{action:(?:edit|activate|delete)}>/{key}", workInProcess).
 		Methods("POST").Name("moderate_post")
 	router.HandleFunc("/id/{id:[0-9]+}/unsubscribe/{email}/{key}>", workInProcess).
 		Methods("GET").Name("unsubscribe")
